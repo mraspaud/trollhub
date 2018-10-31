6608eb2fe9b5a399a2adc28c21a48400f95803a1
Martin Raspaud <martin.raspaud@smhi.se>
2018-10-31 09:24:28 +0100

    Search against local data, with time intervals

--- trollhub/demo.py
+++ trollhub/demo.py
@@ -1,22 +1,31 @@
 import sys
 import random
 from flask import Flask, render_template, jsonify, request
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import json
 from flask import send_file
-
+import os
+# from flask_cas import CAS, login_required
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-db = SQLAlchemy(app)
+# cas = CAS(app, '/cas')
+# app.config['CAS_SERVER'] = 'https://websso-tst.smhi.se'
+# app.config['CAS_AFTER_LOGIN'] = 'root'
+# app.secret_key = 'super secret key'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#
+# db = SQLAlchemy(app)
 
 BASECOORDS = [-13.9626, 33.7741]
 
 
 @app.route('/')
+#@login_required
 def index():
     return render_template('index.html')
+                           #, username=cas.username,
+                                #         display_name=cas.attributes['cas:displayName'])
 
 @app.route('/getFile/<int:file_id>') # this is a job for GET, not POST
 def get_file(file_id):
@@ -32,21 +41,101 @@ def get_file2():
     #print('File requested: %d'%file_id)
     return data
 
-@app.route('/search', methods=['POST'])
-def search():
+@app.route('/searchold', methods=['POST'])
+def searchold():
     from shapely.geometry import shape
     print(request.json)
     js = {'features': [request.json]}
     with open('./testpolygon.geojson') as json_data:
         item = json.load(json_data)
     test_poly = shape(item['features'][0]['geometry'])
+
     for feature in js['features']:
-        polygon = shape(feature['poly']['geometry'])
-        if polygon.intersects(test_poly):
-            print('Found containing polygon:', feature)
-            return jsonify(item)
-        else:
-            return jsonify({})
+        try:
+            polygon = shape(feature['poly']['geometry'])
+            if polygon.intersects(test_poly):
+                print('Found containing polygon:', feature)
+                return jsonify(item)
+            else:
+                return jsonify({})
+        except KeyError:
+            pass
+
+@app.route('/search', methods=['POST'])
+def search():
+    from shapely.geometry import shape, mapping
+    print(request.json)
+    js = request.json
+
+    polygons = []
+    for feature in js['features']:
+        try:
+            polygons.append(shape(feature['geometry']))
+        except KeyError:
+            pass
+
+    start_time = js['start_time']
+    start_time = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+    end_time = js['end_time']
+    end_time = datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+    from shapely.ops import cascaded_union
+    req_poly = cascaded_union(polygons)
+
+    features = []
+    search_funs = {'Local archive': search_local_safe}
+    search_fun = search_funs.get(js['sourceID'], None)
+
+    if search_fun is None:
+        return jsonify({})
+
+    for footprint, properties in search_fun():
+        if (properties['start_time'] > end_time) or (properties['end_time'] < start_time):
+            continue
+        if req_poly.intersects(footprint):
+            filename = properties['uid']
+            features.append(dict(type='Feature', properties={'id': os.path.basename(filename), 'quicklook': os.path.join(filename, 'preview', 'quick-look.png'), 'pass_direction': properties['pass_direction']}, geometry=mapping(footprint)))
+    return jsonify({'features': features})
+
+
+
+def search_local_safe():
+    import xml.etree.ElementTree as ET
+    import glob
+    from shapely import geometry
+    #safe_files = glob.glob('/home/a001673/data/satellite/Sentinel-1/*/*.safe')
+    safe_files = glob.glob('/data/prod/satellit/sentinel1/sar-c/lvl1/*/*.safe')
+    for safe_file in safe_files:
+        my_namespaces = dict([
+              node for _, node in ET.iterparse(
+                  safe_file, events=['start-ns']
+              )
+          ])
+
+        tree = ET.parse(safe_file)
+        root = tree.getroot()
+        #root.find('*//safe:footPrint', my_namespaces)
+        coords = root.find('*//safe:footPrint/gml:coordinates', my_namespaces).text.split()
+        footprint = [[float(x) for x in elt.split(',')] for elt in coords]
+        poly = geometry.Polygon([[p[1], p[0]] for p in footprint])
+        pass_direction = root.find('*//s1:orbitProperties/s1:pass', my_namespaces).text.strip()
+        start_time = root.find('*//safe:acquisitionPeriod/safe:startTime', my_namespaces).text.strip()
+        start_time = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%f')
+        end_time = root.find('*//safe:acquisitionPeriod/safe:stopTime', my_namespaces).text.strip()
+        end_time = datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S.%f')
+        properties = dict(uid=os.path.dirname(safe_file),
+                          pass_direction=pass_direction,
+                          start_time=start_time,
+                          end_time=end_time)
+        yield poly, properties
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    #return 'You want path: %s' % path
+    print('You want path: %s' % path)
+    return send_file(os.path.join('/', path))
 
 if __name__ == '__main__':
+    print(app.instance_path)
     app.run(debug=True)
--- trollhub/static/demo.js
+++ trollhub/static/demo.js
@@ -36,7 +36,7 @@
 var center = [58., 16];
 
 // Create the map
-var map = L.map('map').setView(center, 6);
+var map = L.map('map',{zoomControl: false}).setView(center, 6);
 
 // Set up the OSM layer
 L.tileLayer(
@@ -46,6 +46,7 @@ L.tileLayer(
   }).addTo(map);
 
 var drawnItems = L.featureGroup().addTo(map);
+var segmentItems = L.featureGroup().addTo(map);
 var lastfeature;
 
 
@@ -53,18 +54,23 @@ var lastfeature;
 //L.marker(center).addTo(map);
 
 map.addControl(new L.Control.Draw({
-    edit: {
-        featureGroup: drawnItems,
-        poly : {
-            allowIntersection : false
-        }
-    },
-    draw: {
-        polygon : {
-            allowIntersection: false,
-            showArea:true
-        }
-    }
+  position: 'topright',
+  edit: {
+      featureGroup: drawnItems,
+      poly : {
+          allowIntersection : false
+      }
+  },
+  draw: {
+      polygon : {
+          allowIntersection: false,
+          showArea:true
+      },
+      polyline: false,
+      circle: false,
+      circlemarker: false,
+      marker: false,
+  }
 }));
 //
 // // Initialise the FeatureGroup to store editable layers
@@ -118,6 +124,22 @@ function doWork(event) {
 	// stop link reloading the page
 }
 
+var defaultStyle = {
+            color: "#2262CC",
+            weight: 2,
+            opacity: 0.6,
+            fillOpacity: 0.1,
+            fillColor: "#2262CC"
+        };
+
+var highlightStyle = {
+    color: '#2262CC',
+    weight: 3,
+    opacity: 0.6,
+    fillOpacity: 0.65,
+    fillColor: '#2262CC'
+};
+
 map.on(L.Draw.Event.CREATED, function(e) {
   var type = e.layerType,
     layer = e.layer;
@@ -133,9 +155,25 @@ map.on(L.Draw.Event.CREATED, function(e) {
 function draw_pass(data) {
   L.geoJSON(data, {
     onEachFeature: function (feature, layer) {
-      layer.bindPopup('<h1>'+feature.properties.id+'</h1><p>here a nice quicklook</p>');
-    }
-  }).addTo(map);
+      var style = feature.properties.pass_direction;
+      layer.setStyle(defaultStyle);
+      layer.on("mouseout", function (e) {
+        // Start by reverting the style back
+        layer.setStyle(defaultStyle);
+      });
+      layer.on("mouseover", function (e) {
+        // Start by reverting the style back
+        layer.setStyle(highlightStyle);
+      });
+      switch (feature.properties.pass_direction) {
+        case 'ASCENDING': style = "transform: scaleY(-1);"; break;
+        case 'DESCENDING': style = "transform: scaleX(-1);-webkit-transform: scale(-1, 1);"; break;
+        default: style = feature.properties.pass_direction;
+      }
+      layer.bindPopup('<p>ID: '+feature.properties.id+'</p><img class="quicklook" src="' + feature.properties.quicklook +'" style="'+style+'"/><p>here a nice quicklook and a <a href="'+feature.properties.id+'">download button</a></p>');
+    }//,
+    //style: {color: "#ff2222"}
+  }).addTo(segmentItems);
 }
 
 function searchfun() {
@@ -154,17 +192,23 @@ function searchfun() {
             //         // }
             //       }).addTo(map);
             // });
+            var json_data = drawnItems.toGeoJSON()
+            json_data['sourceID'] = document.forms['searchform']['sourceID'].value
+            json_data['start_time'] = $("#datetimepicker7").datetimepicker('viewDate');
+            json_data['end_time'] = $("#datetimepicker8").datetimepicker('viewDate');
             $.ajax({
               dataType: "json",
               url: '/search',
               type: 'POST',
               contentType : 'application/json',
-              data: JSON.stringify({'poly': lastfeature.toGeoJSON()}),
+              //data: JSON.stringify({'poly': lastfeature.toGeoJSON()}),
+              data: JSON.stringify(json_data),
               //data: JSON.stringify({'bla': 'bli'}),
               success: draw_pass});
+            segmentItems.clearLayers();
             event.preventDefault();
             return false;
           }
 
 
-$('a#test').bind('click', searchfun);
+$('#searchform').submit(searchfun);
