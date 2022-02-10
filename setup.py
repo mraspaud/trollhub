36b42127f1755f0d1898ceb25275ed3e22422b56
Martin Raspaud <martin.raspaud@smhi.se>
2022-02-10 10:30:22 +0100

    Bump up version nummer and add shebang line to start_trollhub

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trollhub',
-    version='0.0.4',
+    version='0.1.0',
     packages=['trollhub'],
     include_package_data=True,
     install_requires=[
