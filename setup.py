a6db7f9c2aa8123878b0c2b3413e5caf03e6ee86
Martin Raspaud <martin.raspaud@smhi.se>
2022-04-12 11:42:38 +0200

    Bump up version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trollhub',
-    version='0.1.1',
+    version='0.1.2',
     packages=['trollhub'],
     include_package_data=True,
     install_requires=[
